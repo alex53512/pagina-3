ed898b62fdbdaedc71e7d7ad5e0e721ca44e1594
yilber <alexpechene572@gmail.com>
2025-12-10 14:55:54 -0500

    Subo mis archivos desde la rama contenido

---
+++ script.js
@@ -0,0 +1,7 @@
+// Texto para la primera imagen
+    document.getElementById("desc1").textContent = 
+        "Descubre la magia de estas imponentes montañas, un destino perfecto para los amantes de la naturaleza y la aventura. Sus paisajes verdes, el aire puro y los senderos naturales ofrecen una experiencia única para desconectarte del estrés y conectarte con la tranquilidad del entorno. Ideal para caminatas, fotografía y actividades al aire libre, este paraíso natural te invita a explorar y vivir momentos inolvidables en plena armonía con la tierra.";
+
+    // Texto para la segunda imagen
+    document.getElementById("desc2").textContent = 
+        "Disfruta de la serenidad de esta espectacular playa, donde el sonido suave de las olas y la brisa del mar crean el escenario ideal para relajarte y renovar energías. Sus aguas cristalinas y su arena dorada te invitan a caminar, nadar y vivir momentos de total tranquilidad. Un destino perfecto para escapadas románticas, vacaciones familiares o simplemente para desconectar bajo el sol y disfrutar de la belleza natural del paraíso.";
